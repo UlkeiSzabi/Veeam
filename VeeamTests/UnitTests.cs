703fd9dcd6e4f716428b2ad67e10d87641ba3484
UlkeiSzabi <ulkei.szabolcs@gmail.com>
2024-08-04 20:06:40 +0300

    Add Constructor tests

--- VeeamTests/UnitTests.cs
+++ VeeamTests/UnitTests.cs
@@ -88,59 +88,5 @@ namespace VeeamTests
             Assert.That(ex.Message, Is.EqualTo("Life Time must be a positive integer greater than zero. Example: 5"));
         }
 
-        [Test]
-        public async Task ExecuteAsync_TerminateProcess_WhenLifetimeExceedsThreshold()
-        {
-            // Arrange
-            var cts = new CancellationTokenSource();
-            var service = new MonitorService(
-                ValidProcessName,
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(5), 
-                CancellationToken.None,
-                _loggerMock.Object);
-
-            // Create and start the process
-            var process = Process.Start(new ProcessStartInfo
-            {
-                FileName = "notepad.exe",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            });
-
-            await Task.Delay(1000); // Give some time for process to start
-
-            // Use reflection to gain access to the protected override method
-            var executeAsyncMethod = typeof(MonitorService)
-            .GetMethod("ExecuteAsync", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-
-            // Execute the ExecuteAsync function
-            var executeTask = (Task)executeAsyncMethod.Invoke(service, new object[] { cts.Token });
-            
-            // Wait more than the provided lifetime
-            await Task.Delay(TimeSpan.FromSeconds(7)); // Wait for enough time for process to be killed
-
-            // Cancel the task
-            cts.Cancel();
-
-            try
-            {
-                process.Kill(); // Ensure the process is terminated if still running
-            }
-            catch(InvalidOperationException) { }
-
-            var processes = Process.GetProcessesByName(ValidProcessName);
-            Assert.That(processes.Length, Is.EqualTo(0), "The process should be killed by now");
-
-            // Verify that logging includes process termination
-            _loggerMock.Verify(l => l.Log(
-                LogLevel.Trace,
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((o, t) => o.ToString().Contains("has been killed")),
-                null,
-                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-                Times.AtLeastOnce);
-        }
     }
 }
